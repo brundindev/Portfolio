7a1e2284b6e3052f4e2f162b85eeebb2c73f11ff
brundindev <thealex847@gmail.com>
2025-03-27 19:03:55 +0100

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,7 +43,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" style={{ width: "75px", height: "auto" }} />
+          <img src={logo} className="img-fluid logo" alt="brand" style={{ width: "50px", height: "50px", objectFit: "contain" }} />
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
